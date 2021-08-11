f2208148376199e6870c8ae62263398d15680cdd
Pavel Petrov <doctorrosen2016@gmail.com>
2021-08-12 01:31:19 +0600

    working with brands

--- src/js/index.js
+++ src/js/index.js
@@ -3,16 +3,19 @@ import "../scss/style.scss";
 import "../img/bg_main.jpg";
 import "./ibg";
 import "../img/logo.png";
+import '../img/generation/01.png';
 
 document.querySelector(".wrapper").classList.add("loaded");
 
-const iconMenu = document.querySelector(".icon-menu");
-
-iconMenu.addEventListener('click', (e) => {
-    const menuBody = document.querySelector('.menu__body');
-    const body = document.querySelector('body');
-
-    e.target.classList.add('active');
-    menuBody.classList.add('active');
-    body.classList.add('lock');
-});
+document.addEventListener("DOMContentLoaded", (e) => {
+    const header = document.querySelector(".header__menu");
+    const icon = document.querySelector('.menu__icon');
+    const body = document.querySelector('.menu__body');
+    header.addEventListener("click", () => {
+      header.classList.toggle('active');
+      icon.classList.toggle('active');
+      body.classList.toggle('active');
+      document.querySelector('body').classList.toggle('lock');
+    });
+  });
+  
